505e330fd9cee7e0972837d8bbd18d8a10975aba
syu-y <syu12580526@gmail.com>
2019-08-19 22:16:01 +0900

    change how to display error message

--- chapter6/src/main/java/com/example/demo/login/controller/SignupController.java
+++ chapter6/src/main/java/com/example/demo/login/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 import java.util.LinkedHashMap;
@@ -32,7 +33,7 @@ public class SignupController {
     }
     // Redirect to login.html
     @PostMapping("/signup")
-    public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model){
+    public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult, Model model){
         if(bindingResult.hasErrors()){
             return getSignUp(form, model);
         }
